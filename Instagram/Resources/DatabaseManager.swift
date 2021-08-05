d32a95e0095f27c6412fabb7e4e69879a791d7c0
anthonycheitzeberg <a.heitzeberg007@gmail.com>
2021-08-05 11:28:01 +0800

    feature: add function to save user on registration controller

--- Instagram/Resources/DatabaseManager.swift
+++ Instagram/Resources/DatabaseManager.swift
@@ -9,8 +9,35 @@ import FirebaseDatabase
 
 public class DatabaseManager {
     static let shared = DatabaseManager()
+    private let database = Database.database().reference()
     
     // MARK: - Public
     
+    /// Check if username and email is available
+    /// - Parameters
+    ///     -email: String representing email
+    ///     -username: String representing username
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
+    }
     
+    /// Inserts new user data to database
+    /// - Parameters
+    ///     -email: String representing email
+    ///     -username: String representing username
+    ///     -completion: Async callback for result if database entry succeeded
+    public func insertNewUser(with email: String, userName: String, completion: @escaping (Bool) -> Void) {
+        let key = email.safeDatabaseKey()
+        database.child(email.safeDatabaseKey()).setValue(["username": userName]) { error, _ in
+            if error == nil {
+                // succeeded
+                completion(true)
+                return
+            } else {
+                // failed
+                completion(false)
+                return
+            }
+        }
+    }
 }
